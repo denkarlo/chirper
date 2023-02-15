389c533a174be3013fce1462d1d615085a17da37
unknown <cit183g-1@M303.UC-BCF.EDU.PH>
2023-02-15 16:48:53 +0800

    put your comment here

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,3 +1,17 @@
+
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <h1>Customers</h1>
+
 @foreach ($customers as $customer)
+<br>
 {{$customer->firstName}}
+<br>
 @endforeach
+
+</body>
+</html> 
