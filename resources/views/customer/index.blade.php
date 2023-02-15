03ed2a8fbf9733e5b1256a66578e520999aaa7b9
unknown <cit183g-1@M303.UC-BCF.EDU.PH>
2023-02-15 17:02:06 +0800

    put your comment here

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,17 +1,17 @@
-
-
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
     <h1>Customers</h1>
-
-@foreach ($customers as $customer)
+    @foreach ($customers as $customer)
 <br>
 {{$customer->firstName}}
 <br>
 @endforeach
-
-</body>
-</html> 
+  </body>
+</html>
